46e45c46a7211600a63bddfe6214dca3f4d7d619
root <quangngovinh6189@gmail.com>
2016-10-22 06:13:37 -0700

    First commit

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/login')
+    def login():
+        return "LOGIN PAGE"
+
+    return app
