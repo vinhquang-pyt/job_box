c3d0382efc90000bf1c72ab5e00486edf2774e64
vinhquang-pyt <quangngovinh6189@gmail.com>
2016-10-23 11:52:40 +0700

    Submit

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template, request
+from flask import *
 from forms import ApplicationForm
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'our_secret_key'
     app.config['CSRF_SESSION_KEY'] = 'THIS IS THE OTHER KEY'
-
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    @app.route('/login')
+    # @app.route('/login')
+    # def login():
+    #     return render_template('login.html')
+
+    @app.route('/hello')
+    def hello():
+        return render_template('hello.html')
+
+    @app.route('/login', methods=['GET', 'POST'])
     def login():
-        return render_template('login.html')
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                return redirect(url_for('hello'))
+        return render_template('login.html', error=error)
 
     @app.route('/apply', methods=['GET', 'POST'])
     def apply():
@@ -20,6 +33,7 @@ def create_app():
         if form.validate_on_submit():
             return 'Thanks for applying'
         return render_template('application.html', form = form)
+
 
 
     return app
