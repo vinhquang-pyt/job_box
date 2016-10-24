ce9daef372dfd344336bcf95a2e0246d88c99965
root <quangngovinh6189@gmail.com>
2016-10-24 03:43:40 -0700

    Submit

--- app/__init__.py
+++ app/__init__.py
@@ -1,52 +1,52 @@
 from flask import *
-from forms import ApplicationForm
+# from forms import ApplicationForm
 from functools import wraps
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'our_secret_key'
-    app.config['CSRF_SESSION_KEY'] = 'THIS IS THE OTHER KEY'
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'our_secret_key'
+app.config['CSRF_SESSION_KEY'] = 'THIS IS THE OTHER KEY'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    def login_required(test):
-        @wraps(test)
-        def wrap(*args, **kwargs):
-            if 'logged_in' in session:
-                return test(*args, **kwargs)
-            else:
-                flash('You need to login first')
-                return redirect(url_for('login'))
-        return wrap
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
 
-    @app.route('/hello')
-    @login_required
-    def hello():
-        return render_template('hello.html')
+@app.route('/hello')
+@login_required
+def hello():
+    return render_template('hello.html')
 
-    @app.route('/logout')
-    def logout():
-        session.pop('logged_in', None)
-        flash('You were logged_out')
-        return redirect (url_for('login'))
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged_out')
+    return redirect (url_for('login'))
 
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        error = None
-        if request.method == 'POST':
-            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-                error = 'Invalid Credentials. Please try again.'
-            else:
-                session['logged_in'] = True
-                return redirect(url_for('hello'))
-        return render_template('login.html', error=error)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('hello'))
+    return render_template('login.html', error=error)
 
-    @app.route('/apply', methods=['GET', 'POST'])
-    def apply():
-        form = ApplicationForm(request.form)
-        if form.validate_on_submit():
-            return 'Thanks for applying'
-        return render_template('application.html', form = form)
+    # @app.route('/apply', methods=['GET', 'POST'])
+    # def apply():
+    #     form = ApplicationForm(request.form)
+    #     if form.validate_on_submit():
+    #         return 'Thanks for applying'
+    #     return render_template('application.html', form = form)
 
-    return app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
